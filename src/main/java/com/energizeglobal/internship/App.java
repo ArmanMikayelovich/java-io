52d1ebc260c1b751b14b88f40e331a92413cf828
Arman Arshakyan <armanar@energizeglobal.com>
2019-12-16 19:40:49 +0400

    Initial commit.

---
+++ src/main/java/com/energizeglobal/internship/App.java
@@ -0,0 +1,21 @@
+package com.energizeglobal.internship;
+
+/**
+ * Hello world!
+ */
+public class App {
+    /**
+     * PROP - see properties of the file
+     * TREE - see all files and inner directories of folder
+     * COPY  - Copy file to directory
+     * MOVE - Move file to directory
+     * RENAME - rename file
+     * READ - read data from file and show in console
+     * WRITE - write data to file
+     */
+    private static final String[] IO_OPERATIONS = {"PROP", "TREE", "COPY", "MOVE", "RENAME", "READ", "WRITE"};
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
